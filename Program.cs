d3cf9c394449fdeedce2997806ec747012cd8307
LiangChen3519 <61409749+LiangChen3519@users.noreply.github.com>
2024-11-10 00:25:02 +0200

    we also tested with new  full timeseries method, and it is ik.

--- Program.cs
+++ Program.cs
@@ -21,5 +21,15 @@ class Program
        };
        var results = dateManager.DateGetter();
        Console.WriteLine($"The begin datetime is {results.Item1}, the end datetime is {results.Item2}");
+       
+       // check the datetime produce
+       var startDate = DateTime.Parse(results.Item1);
+       var endDate = DateTime.Parse(results.Item2);
+       var results2 = dateManager.DateSetter(startDate, endDate,30);
+       foreach (var date in results2)
+       {
+           Console.WriteLine(date);
+       }
+       
     }
 }
